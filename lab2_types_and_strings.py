5cc9ff803402bc5992cee7a9f4886cd3a7d981ef
Eoghan Martin <eoghan2014@gmail.com>
2019-10-16 19:06:11 +0100

    lab3 complete

--- lab2_types_and_strings.py
+++ lab2_types_and_strings.py
@@ -1,8 +1,8 @@
-#course: Object-oriented programming, year 2, semester 1
-#academic year: 201920
-#author: B. Schoen-Phelan
-#date: 29-09-2019
-#purpose: Lab 2
+# course: Object-oriented programming, year 2, semester 1
+# academic year: 201920
+# author: B. Schoen-Phelan
+# date: 29-09-2019
+# purpose: Lab 2
 
 class Types_and_Strings:
     def __init__(self):
@@ -11,54 +11,65 @@ class Types_and_Strings:
     def play_with_strings(self):
         # working with strings
         message = input("Enter your noun: ")
-        print("Originally entered: "+ message)
+        print("Originally entered: " + message)
 
         # print only first and last of the sentence
-
+        print(message[0] + message[-1])
 
         # use slice notation
-
+        print(message[0:len(message):len(message)] + message[-1:len(message):len(message)])
 
         # escaping a character
-
+        print("He said \"That\'s fantastic\"")
 
         # find all a's in the input word and count how many there are
-
+        message2 = input("Please enter a string:")
+        counter = message2.count('a')
+        print("The number of times \'a\' is in this string is " + str(counter))
 
         # replace all occurences of the character a with the - sign
+
         # try this first by assignment of a location in a string and
         # observe what happens, then use replace()
-
+        print(message2.replace('a', '_'))
 
         # printing only characters at even index positions
-
+        print(message[0:len(message):2])
 
     def play_with_lists(self):
         message = input("Please enter a whole sentence: ")
-        print("Originally entered: "+ message)
+        print("Originally entered: " + message)
 
         # hand the input string to a list and print it out
-
+        array = message.split()
+        for i in range(len(array)):
+            print(array[i])
 
         # append a new element to the list and print
-
+        array.append('new')
+        for i in range(len(array)):
+            print(array[i])
 
         # remove from the list in 3 ways
-
+        # array = array[:len(array)-1]
+        # array.remove('sentence')
+        # array.remove(array[-1])
+        print(array)
 
         # check if the word cake is in your input list
-
+        print('cake' in array)
 
         # reverse the items in the list and print
-
+        array.reverse()
+        print(array)
 
         # reverse the list with the slicing trick
-
+        print(array[::-1])
 
         # print the list 3 times by using multiplication
-
+        print(array * 3)
 
 
 tas = Types_and_Strings()
-tas.play_with_strings()
-#tas.play_with_lists()
+# tas.play_with_strings()
+tas.play_with_lists()
--- lab3_word_scramble.py
+++ lab3_word_scramble.py
@@ -15,15 +15,42 @@ class WordScramble:
         print("The user input was: ", self.user_input)
 
         # first scramble is just one word
+        word=list(self.user_input)
+        temp=word[1]
+        word[1]=word[2]
+        word[2]=temp
+        for i in range(len(word)):
+            print(word[i], end ="")
         # reverse two indices
         # particularly good to use is to switch the first two
         # and the last two
         # this only makes sense if you have a world that is longer than 3
+        word='switch'
+        wordlst=list(word)
+        temp1=wordlst[4]
+        temp2=wordlst[5]
+        wordlst[4]=wordlst[0]
+        wordlst[5]=wordlst[1]
+        wordlst[0]=temp1
+        wordlst[1]=temp2
+        print("")
+        for i in range(len(word)):
+            print(wordlst[i], end ="")
+        print("")
+
+
 
 
         # now try to scramble one sentence
         # do just words first, then you can move on to work on
         # punctuation
+        sentence='This is the sentence to scramble.'
+        sentencelst=sentence.split()
+        temp=sentencelst[0]
+        sentencelst[0]=sentencelst[5]
+        sentencelst[5]=temp
+        for i in range(len(sentencelst)):
+            print(sentencelst[i], end=" ")
 
 word_scrambler = WordScramble()
 word_scrambler.scramble()
