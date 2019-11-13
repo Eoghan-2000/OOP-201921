c7fd3fbe8fc0d2c29fdf884e3548085708a59316
Eoghan Martin <eoghan2014@gmail.com>
2019-11-13 13:55:42 +0000

    Lecture Week 9

--- Week9/Lecture_wk9.py
+++ Week9/Lecture_wk9.py
@@ -7,3 +7,25 @@
 #like an abstract idea, you have an idea of how it will work but you aren't sure how you will do it yet
 #lile Scaffolding
 #have to import abc module
+
+#polymorphism
+#the same function ise defined for objects of different types
+
+#encapsulation
+#To hide something
+# __ is the notation
+
+class NonEncapsulated:
+    def __init__(self):
+        self.a=10
+
+n=NonEncapsulated()
+print(n.a)
+
+class Encapsulated:
+    def __init__(self):
+        self.__a=10
+
+e=Encapsulated()
+print(e.__a)
+
