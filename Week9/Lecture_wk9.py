e2dc54804c314b056ca60c8a5c7f88905d3874b1
Eoghan Martin <eoghan2014@gmail.com>
2019-11-13 13:38:19 +0000

    Organising

---
+++ Week9/Lecture_wk9.py
@@ -0,0 +1,9 @@
+#Composition
+#Creates a part-of relationship
+#e.g. home --> room
+#game design
+
+#abstract classes
+#like an abstract idea, you have an idea of how it will work but you aren't sure how you will do it yet
+#lile Scaffolding
+#have to import abc module
